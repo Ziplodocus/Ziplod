d1f43d851cc3691e20531476646bc4ed5cdd4b32
Ziplodocus <zirpho@gmail.com>
2021-11-26 23:09:39 +0000

    Reset exposed token and remove configs from repo

--- data/config.ts
+++
@@ -1,7 +0,0 @@
-
-export const prefix:'!' = "!";
-export const token:string = "NzA1MDQ1MjMyMjczNzE5MzI3.Xql-ig.giKQKLUTKOkTtAz1OwBR1rF9ARY";
-export enum ThemeTro {
-    in = 'in',
-    out = 'out'
-}
