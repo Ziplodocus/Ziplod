663bf56a658729e939058b2a5f118bb8d58e551b
Ziplodocus <zirpho@gmail.com>
2022-08-21 19:48:40 +0100

    Add response when using speak with no args

--- src/events/messageCreate/commands/defined/speak.ts
+++ src/events/messageCreate/commands/defined/speak.ts
@@ -5,8 +5,11 @@ export default async function (msg: ExtendedMessage) {
   const voiceChan = msg.voiceChannel;
   if (!voiceChan) {
     return msg.message.reply(
-      "\n Someone has to be in a voice channel don' they? idiot.",
+      "Someone has to be in a voice channel don' they? idiot.",
     );
+  }
+  if (!msg.args[0]) {
+    return msg.message.reply("What must I say oh fascist overlord?");
   }
   const stringToSpeak = msg.args.filter((arg) => !arg.startsWith("<@")).join(
     " ",
