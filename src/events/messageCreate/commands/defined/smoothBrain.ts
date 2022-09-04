6e809e62c563aa0b50e08352b6ca7efa19e58fea
Zirpho <zirpho@gmail.com>
2022-09-04 15:57:11 +0100

    Much smoother

--- src/events/messageCreate/commands/defined/smoothBrain.ts
+++ src/events/messageCreate/commands/defined/smoothBrain.ts
@@ -2,6 +2,6 @@ import ExtendedMessage from "../../../../classes/ExtendedMessage.js";
 
 
 export default async function( msg: ExtendedMessage ) {
-    msg.message.reply( 'Smoother than yours.' );
+    msg.message.reply( 'Much smoother than yours.' );
     return msg;
 };
